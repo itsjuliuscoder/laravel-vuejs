98af0295bb8346f51926c3bdb3f9cd3d13d66bcf
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2020-01-27 14:23:38 +0100

    Laravel Vue.js Application

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+# Use it to help set default string length
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //sets a default string length
+        Schema::defaultStringLength(191);
+    }
+}
